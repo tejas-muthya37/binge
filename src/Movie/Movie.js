0fc0eed487bc135571175e03eedb02e88632e3f2
tejas-muthya37 <55662912+tejas-muthya37@users.noreply.github.com>
2022-03-24 09:29:43 +0530

    Feat: Added Modal from MUI

--- src/Movie/Movie.js
+++ src/Movie/Movie.js
@@ -5,8 +5,30 @@ import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 import ThumbDownIcon from "@mui/icons-material/ThumbDown";
 import DeleteIcon from "@mui/icons-material/Delete";
 import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { useState } from "react";
 
 const Movie = (props) => {
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "none",
+    outline: "none",
+    color: "black",
+    boxShadow: 24,
+    p: 4,
+  };
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <div className="Movie">
       <div onClick={props.addToHistory}>
@@ -25,7 +47,22 @@ const Movie = (props) => {
           {props.historyPage && (
             <DeleteIcon onClick={props.removeFromHistory} fontSize="small" />
           )}
-          <PlaylistAddIcon />
+          <PlaylistAddIcon onClick={handleOpen} />
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Text in a modal
+              </Typography>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+              </Typography>
+            </Box>
+          </Modal>
         </div>
       </div>
     </div>
